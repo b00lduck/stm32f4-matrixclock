ffee0a330134612151345ea5965e7cc7526fc98d
Daniel Zerlett <daniel@zerlett.eu>
2016-10-08 17:17:49 +0200

    Complete GSCLK/BLANK signal generation

--- tlc5940/driver.c
+++ tlc5940/driver.c
@@ -2,13 +2,12 @@
 #include "driver.h"
 
 
-
 void tlc5940_init(void) {
 	tim1_init();
 	tim3_init();
 }
 
-
+#define FRAMERATE_FACTOR 0x0040
 
 void tim1_init(void) {
 
@@ -23,7 +22,7 @@ void tim1_init(void) {
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
 	GPIO_Init(SIG_BLANK_PORT, &GPIO_InitStructure);
@@ -33,8 +32,7 @@ void tim1_init(void) {
 
 
 	// Configure the timer
-	// Generate BLANK signals
-	// Trigger TIM3 (GSCLK generation)
+	// Generate GSCLK signals
 
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
@@ -42,21 +40,23 @@ void tim1_init(void) {
 	//NVIC_InitTypeDef NVIC_InitStructure;
 
 	// Time base configuration
-	TIM_TimeBaseStructure.TIM_Prescaler = 0x00a0;
+	TIM_TimeBaseStructure.TIM_Prescaler = FRAMERATE_FACTOR;
+	TIM_TimeBaseStructure.TIM_Period = 0x0001;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseStructure.TIM_Period = 0x103c;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV4;
-	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(SIG_BLANK_TIMER, &TIM_TimeBaseStructure);
 
 	// PWM1 Mode configuration: Channel1
-	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-	TIM_OCInitStructure.TIM_Pulse = 50;
+	TIM_OCInitStructure.TIM_Pulse = 0x0001;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
 	TIM_OC1Init(SIG_BLANK_TIMER, &TIM_OCInitStructure);
+
+	// Be a slave of SIG_BLANK_TIMER
+	TIM_SelectInputTrigger(SIG_BLANK_TIMER, TIM_TS_ITR2);
+	TIM_SelectSlaveMode(SIG_BLANK_TIMER, TIM_SlaveMode_Gated);
+	TIM_SelectMasterSlaveMode(SIG_BLANK_TIMER, TIM_MasterSlaveMode_Enable);
 
 	// enable the counter
 	TIM_Cmd(SIG_BLANK_TIMER, ENABLE);
@@ -78,7 +78,7 @@ void tim3_init(void) {
 	// GPIO configuration
 	GPIO_InitStructure.GPIO_Pin = SIG_GSCLK_PIN;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
 	GPIO_Init(SIG_GSCLK_PORT, &GPIO_InitStructure);
@@ -87,61 +87,46 @@ void tim3_init(void) {
 
 
 	// Configure the timer
-	// Generate GSCLK signals
+	// Generate BLANK signals
 
 	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
 	TIM_OCInitTypeDef  TIM_OCInitStructure;
 	NVIC_InitTypeDef NVIC_InitStructure;
 
 	// Time base configuration
-	TIM_TimeBaseStructure.TIM_Prescaler = 0x00a0;
-	TIM_TimeBaseStructure.TIM_Period = 0x0002;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV4;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_Prescaler = FRAMERATE_FACTOR;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_CenterAligned1;
+	TIM_TimeBaseStructure.TIM_Period = 0x1030;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(SIG_GSCLK_TIMER, &TIM_TimeBaseStructure);
 
 	// PWM1 Mode configuration: Channel1
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OCInitStructure.TIM_Pulse = 0x0001;
+	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+	TIM_OCInitStructure.TIM_Pulse = 0x20;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
 	TIM_OC1Init(SIG_GSCLK_TIMER, &TIM_OCInitStructure);
+
+	// OC Mode configuration: Channel2
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Disable;
+	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+	TIM_OCInitStructure.TIM_Pulse = 0x30;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+	TIM_OC2Init(SIG_GSCLK_TIMER, &TIM_OCInitStructure);
+
+	TIM_SelectOutputTrigger(SIG_GSCLK_TIMER, TIM_TRGOSource_OC2Ref);
+	TIM_SelectMasterSlaveMode(SIG_GSCLK_TIMER, TIM_MasterSlaveMode_Enable);
+
 
 	// enable the counter
 	TIM_Cmd(SIG_GSCLK_TIMER, ENABLE);
 
+	// Main Output Enable
+	TIM_CtrlPWMOutputs(SIG_GSCLK_TIMER, ENABLE);
 
 }
-
-
-
-/*
-void TIM1_CC_IRQHandler(void) {
-
-	if (videoInstance.textMode == 1) {
-		HsyncIsrTextMode();
-	} else {
-		HsyncIsrPixelMode();
-	}
-
-	// reset interrupt flag
-	HSYNC_TIMER->SR = (uint16_t)~TIM_IT_CC2;
-
-	// increment line
-	videoInstance.current_y++;
-
-	for(uint8_t i=0; i < videoInstance.switchToBwAtLineSize; i++) {
-		if(videoInstance.current_y == videoInstance.switchToBwAtLine[i]) {
-			VIDEO_SWITCH_PORT->BSRRL = GPIO_Pin_1;
-		}
-	}
-
-	for(uint8_t i=0; i < videoInstance.switchToColorAtLineSize; i++) {
-		if(videoInstance.current_y == videoInstance.switchToColorAtLine[i]) {
-			VIDEO_SWITCH_PORT->BSRRH = GPIO_Pin_1;
-		}
-	}
-
-}
-*/
-
