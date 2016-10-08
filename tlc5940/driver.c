ecbca7b9f49373b7a6a50e890e91e726173fb8d5
Daniel Zerlett <daniel@zerlett.eu>
2016-10-08 18:06:46 +0200

    tighten timing

--- tlc5940/driver.c
+++ tlc5940/driver.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include "driver.h"
 
+#define CLOCK_PRESCALE 0x0040
+#define DELAY_GSCLK 0x001
+#define BLANK_LENGTH 0x0004
+#define LATCH_LENGTH 0x0002
 
 void tlc5940_init(void) {
 	tim1_init();
 	tim3_init();
 }
-
-#define FRAMERATE_FACTOR 0x0040
 
 void tim1_init(void) {
 
@@ -40,7 +42,7 @@ void tim1_init(void) {
 	//NVIC_InitTypeDef NVIC_InitStructure;
 
 	// Time base configuration
-	TIM_TimeBaseStructure.TIM_Prescaler = FRAMERATE_FACTOR;
+	TIM_TimeBaseStructure.TIM_Prescaler = CLOCK_PRESCALE;
 	TIM_TimeBaseStructure.TIM_Period = 0x0001;
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
@@ -82,9 +84,15 @@ void tim3_init(void) {
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
 	GPIO_Init(SIG_GSCLK_PORT, &GPIO_InitStructure);
-
 	GPIO_PinAFConfig(SIG_GSCLK_PORT, SIG_GSCLK_PINSOURCE, SIG_GSCLK_PIN_AF);
 
+	GPIO_InitStructure.GPIO_Pin = SIG_LATCH_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+	GPIO_Init(SIG_LATCH_PORT, &GPIO_InitStructure);
+	GPIO_PinAFConfig(SIG_LATCH_PORT, SIG_LATCH_PINSOURCE, SIG_LATCH_PIN_AF);
 
 	// Configure the timer
 	// Generate BLANK signals
@@ -94,34 +102,41 @@ void tim3_init(void) {
 	NVIC_InitTypeDef NVIC_InitStructure;
 
 	// Time base configuration
-	TIM_TimeBaseStructure.TIM_Prescaler = FRAMERATE_FACTOR;
+	TIM_TimeBaseStructure.TIM_Prescaler = CLOCK_PRESCALE;
 	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_CenterAligned1;
-	TIM_TimeBaseStructure.TIM_Period = 0x1030;
+	TIM_TimeBaseStructure.TIM_Period = 0x1000 + BLANK_LENGTH + DELAY_GSCLK;
 	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(SIG_GSCLK_TIMER, &TIM_TimeBaseStructure);
 
-	// PWM1 Mode configuration: Channel1
+	// PWM1 Mode configuration: Channel1 (BLANK)
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-	TIM_OCInitStructure.TIM_Pulse = 0x20;
+	TIM_OCInitStructure.TIM_Pulse = BLANK_LENGTH;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
 	TIM_OC1Init(SIG_GSCLK_TIMER, &TIM_OCInitStructure);
 
-	// OC Mode configuration: Channel2
+	// OC Mode configuration: Channel2 (LATCH)
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
-	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Disable;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
 	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
-	TIM_OCInitStructure.TIM_Pulse = 0x30;
+	TIM_OCInitStructure.TIM_Pulse = LATCH_LENGTH;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
 	TIM_OC2Init(SIG_GSCLK_TIMER, &TIM_OCInitStructure);
 
-	TIM_SelectOutputTrigger(SIG_GSCLK_TIMER, TIM_TRGOSource_OC2Ref);
+	// OC Mode configuration: Channel3 (gate for GSCLK)
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Disable;
+	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;
+	TIM_OCInitStructure.TIM_Pulse = BLANK_LENGTH + DELAY_GSCLK;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+	TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Reset;
+	TIM_OC3Init(SIG_GSCLK_TIMER, &TIM_OCInitStructure);
+	TIM_SelectOutputTrigger(SIG_GSCLK_TIMER, TIM_TRGOSource_OC3Ref);
 	TIM_SelectMasterSlaveMode(SIG_GSCLK_TIMER, TIM_MasterSlaveMode_Enable);
-
 
 	// enable the counter
 	TIM_Cmd(SIG_GSCLK_TIMER, ENABLE);
