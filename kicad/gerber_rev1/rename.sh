e168432c81e74e61d5018bbef655b0ca4f4cf3ac
Daniel Zerlett <d.zerlett@tarent.de>
2016-10-19 09:58:43 +0200

    pcb fixes

---
+++ kicad/gerber_rev1/rename.sh
@@ -0,0 +1,9 @@
+cp matrix-clock-B.Cu.gbr matrixclock.GBL
+cp matrix-clock-B.Mask.gbr matrixclock.GBS
+cp matrix-clock-B.SilkS.gbr matrixclock.GBO
+cp matrix-clock-F.Cu.gbr matrixclock.GTL
+cp matrix-clock-F.Mask.gbr matrixclock.GTS
+cp matrix-clock-F.SilkS.gbr matrixclock.GTO
+cp matrix-clock.drl matrixclock.TXT
+zip gerber.zip matrixclock.*
+rm matrixclock.*
